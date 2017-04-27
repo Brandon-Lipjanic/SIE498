74bce906cea8422e868ea917097d93f7cda40101
Brandon-Lipjanic <blipjanic1616@email.arizona.edu>
2017-04-27 15:00:02 -0700

    Added code for testing door latch

---
+++ DoorLatchTest/DoorLatchTest.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+#include <Servo.h>
+#include <SPI.h>
+#include <Pixy.h>
+#include <math.h>
+
+static const int doorLatch = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(doorLatch,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+delay(5000);
+digitalWrite(doorLatch,HIGH);
+delay(5000);
+digitalWrite(doorLatch,LOW);
+}
